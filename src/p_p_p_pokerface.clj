3c37871af4dba86b5d4c820ae374a2cc83c507ef
Conor Futro <cfutro@fluxorum.com>
2022-12-28 17:00:48 -0700

    solved exercises

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,52 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[r _] card]
+    (def values {\T 10, \J 11, \Q 12, \K 13, \A 14})
+    (if (Character/isDigit r)
+      (Integer/valueOf (str r))
+      (get values r))))
 
 (defn suit [card]
-  nil)
+  (let [[_ s] card]
+    (str s)))
 
 (defn pair? [hand]
-  nil)
+  (contains? (set (vals (frequencies (map rank hand)))) 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (contains? (set (vals (frequencies (map rank hand)))) 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (contains? (set (vals (frequencies (map rank hand)))) 4))
 
 (defn flush? [hand]
-  nil)
+  (apply = (map suit hand)))
 
 (defn full-house? [hand]
-  nil)
+  (and (pair? hand) (three-of-a-kind? hand)))
 
 (defn two-pairs? [hand]
-  nil)
+  (= (get (frequencies (vals (frequencies (map rank hand)))) 2) 2))
 
 (defn straight? [hand]
-  nil)
+  (let [ranks (map rank hand)]
+    (if (= (range (apply min ranks) (+ (apply min ranks) 5)) (sort ranks))
+      true
+      (= (range (apply min (replace {14 1} ranks)) (+ (apply min (replace {14 1} ranks)) 5)) (sort (replace {14 1} ranks))))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
+
+(defn high-card? [hand]
+  true)
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[high-card? 0] [pair? 1] [two-pairs? 2]
+                   [three-of-a-kind? 3] [straight? 4]
+                   [flush? 5] [full-house? 6]
+                   [four-of-a-kind? 7] [straight-flush? 8]}
+        hand-types (fn [checker] (if ((first checker) hand)
+                                   (second checker)
+                                   0))]
+    (apply max (map hand-types checkers)))) 
